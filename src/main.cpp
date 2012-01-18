7b2eed51c82d62956589f1db30c1ded73b3cd3ed
Christopher Birger <chrbi049@student.liu.se>
2012-01-18 12:52:49 +0100

    Project setup

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "glew.h"
+#include "wglew.h"
+#include "glfw.h"
+
+#include "math3d.h"
+#include "GLFrame.h"
+#include "GLMatrixStack.h"
+#include "GLFrustum.h"
+#include "GLGeometryTransform.h"
+
+int main(void)
+{
+	std::cout << "Hello world" << std::endl;
+
+	std::cin.get(); //Pauses the system until user gives an input
+
+	return 0;
+}
